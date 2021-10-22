7b2f1eea68950473b0c44bbf9aa73427ebb18993
Vishal062 <vs9425348@gmail.com>
2021-10-23 02:48:57 +0530

    Pagination and email assg done

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const sendmail= require('../utils/sendmail');
+const router = express.Router();
+const User= require('../model/user.model');
+//nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+
+router.get("",async function (req, res){
+    const page= +req.query.page || 1;
+    const size = +req.query.size || 10;
+    const offset = (page-1)*size;
+    const user = await User.find().lean().skip(offset).limit(size).exec();
+    const totalUserCount = await User.find().countDocuments(); 
+    const totalpages= Math.ceil(totalUserCount/size);
+    
+    sendmail({
+        from: "vishal@masai.com",
+        to: "xyz@flipkart.com",
+        subject: "Message",
+        text: "Plaintext version of the message",
+        html: "<p>Hello from vishal 2345678 MVC Pattern 123 TRIPOD</p>"
+    });
+    
+    // message config portion 
+    
+
+
+    // const transporter = nodemailer.createTransport({
+    //     host: CURRENT_ENVIRONMENT == "development" ? "smtp.mailtrap.io" : "",
+    //     port: 587,
+    //     secure: false, // upgrade later with STARTTLS
+    //     auth: {
+    //       user: SMTP_USERNAME,
+    //       pass: SMTP_PASSWORD,
+    //     },
+    // });
+    
+   // sending the email through nodemailer 
+   
+ 
+
+    console.log(totalpages);
+   return res.status(200).send({user,totalpages});
+});
+
+
+module.exports = router;
